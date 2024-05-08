07e6c4c9fa8ae951bedb19d80429acc5273d6903
Gengata <154549793+Gengata@users.noreply.github.com>
2024-05-08 08:25:07 +0300

    Add files via upload

---
+++ 1902/1902/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1902
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //vuvejdane
+            int[] ime = new int[5];
+            string[] nomer = new string[5];
+            for (int i = 0; i <= 5; i++)
+            {
+                nomer[i] = Console.ReadLine();
+                Console.WriteLine("vuvedi nomer");
+                ime[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine("ime");
+            }
+
+            //izhod
+            for (int i = 0; i <= 5; i++)
+
+            {
+                Console.WriteLine(ime[i] + " " + nomer[i]);
+
+
+            }
+            for (int i = 0; i >= 3; i++)
+            {
+                if (i < 3)
+                Console.WriteLine(nomer[i] + ime[i]);
+            }
+            Console.Write("kakvo ime che tursq");
+            for (int i = 0; i <= 5; i++)
+            {
+                string name = Console.ReadLine();
+                if (name == "ivan")
+                  else
+                        {
+
+                        }
+        }
+             
+}
+}
---
+++ 1902/zadelane na pamet/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadelane_na_pamet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //zavejdane na pamet
+            double[] pari = new double[2];
+            string[] vid_zakuska = new string[2];
+            //vhod
+           
+            for (int i = 0; i < 2; i++)
+            {
+
+                Console.WriteLine("vuvedi zakuska");
+                vid_zakuska[i] = Console.ReadLine();
+                
+                Console.Write("vuvedi pari");
+                pari[i] = double.Parse(Console.ReadLine());
+            }
+            //izhod
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine(vid_zakuska[i] + "/" + pari[i]);
+            }
+        }
+    }
+}
