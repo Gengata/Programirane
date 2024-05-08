dbaa0e4239f8a045d179bcf43d9d769a1d9ad60d
Gengata <154549793+Gengata@users.noreply.github.com>
2024-05-08 08:31:53 +0300

    Add files via upload

---
+++ 2703/2703/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2703
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vuvedi teks:");
+            string str2 = Console.ReadLine();
+            for (int i= str2.Length - 1;i>=0;i--)
+            {
+
+                Console.WriteLine(str2[i]);    
+            }
+            
+        }
+    }
+}
