3c97b2aa42f758b76070fcc27559b843af546aae
Gengata <154549793+Gengata@users.noreply.github.com>
2024-05-08 08:42:24 +0300

    Add files via upload

---
+++ zad_s_gospodina/nzzzzzzzz/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nzzzzzzzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] chisla = new int[n];
+            int sum = 0;
+            for (int i = 0; i <n;i++)
+            {
+                Console.WriteLine("vuvedi chislo");
+                chisla[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0;i <= chisla.Length - 1; i++)
+            {
+                sum += i * chisla[i];
+            }
+            Console.WriteLine("sum is {0}",sum);
+        }  
+    }
+}
---
+++ zad_s_gospodina/zad_s_gospodina/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad_s_gospodina
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] chisla = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("vuvedi");
+                chisla[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                if (chisla[i] % 2 == 0)
+                {
+                    Console.WriteLine(chisla[i]);
+                }
+                else
+                {
+                    Console.WriteLine("grehka");
+                }
+            }
+        }
+    }
+}
