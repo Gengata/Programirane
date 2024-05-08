788bb056ef4dcd9c5253da86dd4b10582361bf54
Gengata <154549793+Gengata@users.noreply.github.com>
2024-05-08 08:32:53 +0300

    Add files via upload

---
+++ ora = 2604/ora = 2604/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ora___2604
+{
+    internal class Person
+    {
+        public string name;
+        public int age;
+        public string grad;
+        public string name1;
+        public int age1;
+        public string grad1;
+
+
+
+        public string Name
+        {
+            get { return name; }    
+            set { name = value; }   
+        }
+        public int Age
+        {
+            get { return age; } 
+            set { age = value; }    
+        }
+        public string Grad
+        {
+            get { return grad; }
+            set { grad = value; }
+          
+        }
+        public string Name1
+        {
+            get { return name1; }
+            set { name1 = value; }
+        }
+        public int Age1
+        {
+            get { return age1; }
+            set { age1 = value; }
+        }
+        public string Grad1
+        {
+            get { return grad1; }
+            set { grad1 = value; }
+
+        }
+    }
+}
---
+++ ora = 2604/ora = 2604/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ora___2604
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Console.WriteLine("N=");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("kak se kazvah");   
+                person.name = Console.ReadLine();
+                Console.WriteLine("vuvedi godini");
+                person.age = int.Parse(Console.ReadLine());
+                Console.WriteLine("ot koi grad si");
+                person.grad = Console.ReadLine();
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(person.name + person.age + person.grad);
+               
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("kak se kazvah");
+                person.name1 = Console.ReadLine();
+                Console.WriteLine("vuvedi godini");
+                person.age1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("ot koi grad si");
+                person.grad1 = Console.ReadLine();
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(person.name1 + person.age1   + person.grad1);
+
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (person.grad == "Sopot")
+                {
+                    Console.WriteLine(person.name + person.age + person.grad);
+                   
+                }
+                else
+                {
+                    Console.WriteLine("nqma takiv ot nacho selo");
+                }
+            }
+        }
+    }
+}
