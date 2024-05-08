2c5e88c76970b1c642295f8375d5937af8b211e5
Gengata <154549793+Gengata@users.noreply.github.com>
2024-05-08 08:25:58 +0300

    Add files via upload

---
+++ 2102/2102/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2102
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vuvedi kolko 4isla che vuvejdah");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("kolko pari che zavurtvam");
+            int k = int.Parse(Console.ReadLine());
+            
+
+            int[] number = new int[n];
+            int[] sum = new int[n];
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("vuvedi 4isla");
+                number[i] = int.Parse(Console.ReadLine());
+                //sum[i] = 0;
+                
+            }
+
+            //ihhod 3
+            Console.WriteLine("vuvedi cisla");
+            for (int i = 0; i <4; i++)
+            {
+                Array.Reverse.Sort
+                Array.Sort(number); 
+            }
+            Console.WriteLine("ihhod bez posleden element");
+
+
+
+
+
+
+
+
+
+
+            //sum[n] = 0;
+            //for (int r = 1; r < n;r++)
+            //{
+
+            //    for(int i = 0; i<n-1;i++)
+            //    {
+            //        sum[i] = sum[i] + r + i;
+            //        sum[i] = sum[i] + r;
+            //    }
+            //}
+            //Console.WriteLine(sum[n]);
+            //for (int r = 1; r < n; r++)
+            //{
+
+        }
+
+
+
+        }
+    }
+
