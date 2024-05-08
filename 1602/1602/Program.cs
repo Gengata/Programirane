1458f1732c75fb40d3fe0f1e55543173416c3eb3
Gengata <154549793+Gengata@users.noreply.github.com>
2024-05-08 08:24:14 +0300

    Add files via upload

---
+++ 1602/1602/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1402
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            string[] ime = new string[n];
+            string[] imena;
+            //i sega vhod
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("vuvedi ime:");
+                ime[i] = Console.ReadLine();
+                imena = Console.ReadLine().Split(' ').ToArray();
+               
+                Console.WriteLine(" purvo IME[{0}] = {1}", i, imena[i]);
+                Console.WriteLine("vtoro IME[{0}] = {1}", i + 1, imena[i + 1]);
+                Console.WriteLine("treto IME[{0}] = {1}", i + 2, imena[i + 2]);
+            }
+
+            // izhod 2 na elementi ot posledniq do purviq
+            for (int i = n - 1; i >= 0; i--)
+            {
+                imena = Console.ReadLine().Split(' ');
+                Console.WriteLine("IME[{0}] = {1}", i, imena[i]);
+                Console.WriteLine("IME[{0}] = {1}", i, ime[i]);
+                Console.WriteLine("IME[{0}] = {1}", i, ime[i]);
+            }
+            // izhod 3 
+            for (int i = n - 1; i >= 0; i--)
+            {
+                imena = Console.ReadLine().Split(' ');
+                Console.WriteLine("IME[{0}] = {1}", i , imena[i]);
+                Console.WriteLine("IME[{0}] = {1}", i + 2, ime[i]);
+                Console.WriteLine("IME[{0}] = {1}", i + 3, ime[i]);
+            }
+
+
+
+
+
+        }
+    }
+}
