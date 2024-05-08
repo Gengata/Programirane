2cf0ede984d7c67892269c12d2cc441c43631fb5
Gengata <154549793+Gengata@users.noreply.github.com>
2024-05-08 08:17:25 +0300

    Add files via upload

---
+++ 22_04/22_04/BankAcc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_04
+{
+    internal class BankAcc
+    {
+        private int id;
+        private double balance;
+
+        public int Id
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+
+        public double Balance
+        {
+            get { return this.Balance; }
+            set { this.Balance = value; }
+        }
+        public void Deposit(double amount)
+        {
+        this.balance += amount;
+        }
+        public void Withdraw(double amount)
+         {
+            this.balance -= amount;
+         }
+        public override string ToString()
+        {
+            return $"Account {this.Id} balance{this.balance}";
+        }
+
+
+
+    }
+}
---
+++ 22_04/22_04/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAcc acc = new BankAcc();    
+            var acc1 = new BankAcc();
+
+            //acc.Id = 1;
+            //acc.Deposit(20);
+            //acc.Withdraw(30);
+            //Console.WriteLine(acc.ToString());  
+
+
+
+            var accounds = new Dictionary<int, BankAcc>();
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+               
+                
+             var cmdArgs = command.Split(' ');
+                var id = int.Parse(cmdArgs[1]);
+                if (accounds.ContainsKey(id)) { Console.WriteLine("Accounds alredy exist");}
+                else
+                {
+                    var acc2 = new BankAcc();
+                    acc.Id = id;    
+                    accounds.Add(id, acc2); 
+                }
+
+               
+                var cmdType = cmdArgs[0];
+                switch (cmdType) 
+                {
+                    case "Create":Create(cmdArgs,accounds); break;
+                    case "Deposit": Deposit(cmdArgs, accounds); break;
+                    case "Withdraw":Withdraw(cmdArgs, accounds); break;
+                    case "Print": Print(cmdArgs, accounds); break;
+                }
+            }
+        }
+    }
+}
