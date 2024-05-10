fd0f0b9cc0edfb8f14efe38506db97a0d3c821ff
Gengata <154549793+Gengata@users.noreply.github.com>
2024-05-10 12:02:25 +0300

    Add files via upload

---
+++ klasove/klasove/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasove
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+                
+            Console.WriteLine("kak se kazvah");
+            string name = Console.ReadLine();   
+            
+            ime ime = new ime();
+            Console.WriteLine("zdraveite az sum " + name + " i sum mnogo qk");
+        }
+    }
+}
---
+++ klasove/klasove/ime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasove
+{
+    internal class ime
+    {
+        private string aut;
+        private string name;
+        public string Name
+        {
+            get { return name; }    
+            set { name = value; }   
+        }
+        public void Aut()
+        {
+            Console.WriteLine("zdraveite az sum" + name + "i sum mnogo qk");  
+            
+        }
+    }
+}
